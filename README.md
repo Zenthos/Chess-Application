518e37d2dae8eadbd00670f57e32abe724a451ff
Zenthos <20268714+Zenthos@users.noreply.github.com>
2019-11-29 11:54:11 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# A Chess Application
+A Chess application allowing you to player with other people, and talk through chat rooms.
+
+## Built Using
+1. Node.js
+2. Socket.io
+3. Express
+4. JQuery
+
+## To Do List
+- [ ] Run all chess actions through the server
+- [ ] Implement turn switch through server
+- [ ] Finish king piece class
+- [ ] Add win/draw conditions
+- [ ] Create chat rooms
+- [ ] Create homepage
+- [ ] Allow users to play from multiple computers
+- [ ] Add Cross-Platform Support
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](https://github.com/Zenthos/Chess-Application/blob/master/LICENSE) file for details
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.use('/static', express.static(path.join(__dirname, '/public')));
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', function(socket){
+    io.emit('chat message', "A User has Connected");
+
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+    });
+
+    socket.on('piece captured', function(piece) {
+        console.log(`${piece.player} ${piece.pieceType}, was captured!`);
+    });
+
+    socket.on('player moved', function(piece) {
+        console.log(`${piece.player} ${piece.pieceType}, has moved!`);
+    });
+
+    socket.on('disconnect', function(){
+        io.emit('chat message', "A User has Disconnected");
+    });
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
